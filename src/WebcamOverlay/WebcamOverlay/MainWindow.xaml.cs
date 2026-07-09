83f2672b37edcd7142025d5443ae239d4ab4a0e4
MingulovTA <mingulov.t@yandex.ru>
2026-07-09 12:59:09 +0700

    feat(ui): add camera selection screen

--- src/WebcamOverlay/WebcamOverlay/MainWindow.xaml.cs
+++ src/WebcamOverlay/WebcamOverlay/MainWindow.xaml.cs
@@ -1,32 +1,24 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using WebcamOverlay.Camera;
 using WebcamOverlay.Helpers;
 
 namespace WebcamOverlay
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private readonly CameraService _cameraService = new CameraService();
-        
+
         public MainWindow()
         {
             InitializeComponent();
-
             _cameraService.FrameReceived += OnFrameReceived;
-            var cameras = _cameraService.GetAvailableCameras();
-
-            if (cameras.Count > 0)
-            {
-                _cameraService.Start(cameras[0]);
-            }
+            ShowCameraSelection();
         }
-        
+
         protected override void OnClosed(EventArgs e)
         {
             _cameraService.FrameReceived -= OnFrameReceived;
@@ -34,7 +26,31 @@ namespace WebcamOverlay
 
             base.OnClosed(e);
         }
+
+        private void ShowCameraSelection()
+        {
+            CameraListPanel.Children.Clear();
+            var cameras = _cameraService.GetAvailableCameras();
+            foreach (var camera in cameras)
+            {
+                var button = new Button
+                {
+                    Content = camera.Name, Margin = new Thickness(0, 0, 0, 10), Padding = new Thickness(10),
+                    FontSize = 18
+                };
+                button.Click += (sender, args) => StartCamera(camera);
+                CameraListPanel.Children.Add(button);
+            }
+        }
         
+        private void StartCamera(CameraInfo camera)
+        {
+            CameraListPanel.Visibility = Visibility.Collapsed;
+            CameraImage.Visibility = Visibility.Visible;
+
+            _cameraService.Start(camera);
+        }
+
         private void OnFrameReceived(Bitmap bitmap)
         {
             Dispatcher.BeginInvoke(new Action(() =>
